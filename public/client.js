b19e399b1f9b87ffb1dfb3a9a7d1fb35a5cab7d0
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-12-04 15:40:34 -0500

    Check to see if postgres connects

--- public/client.js
+++ public/client.js
@@ -11,4 +11,16 @@ function scrollToSection(sectionId) {
 let button = document.getElementById('searchButton');
 document.addEventListener('click', () => {
   console.log('Button was clicked');
+
+  fetch('/clicked', {method: 'POST'})
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
