0197790165a7361757b6f4c228995675fd4a669e
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-12-03 21:11:32 -0500

    MongoDB installation

--- public/client.js
+++ public/client.js
@@ -1,6 +1,9 @@
 console.log('Client-side code running');
 
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-});
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}  
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 console.log('Server-side code running');
 
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 3000;
-
-// serve files from the public directory
 app.use(express.static('public'));
+//const uri = "mongodb+srv://Mcen25:ZCsyZipn0J1fpbnW@constellations.ketcgbo.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Mcen25:ZCsyZipn0J1fpbnW@constellations.ketcgbo.mongodb.net/?retryWrites=true&w=majority";
+//Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
 
-// start the express web server listening on 8080
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log('listening on 3000');
 });
