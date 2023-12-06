38578a667474fb6a84e1430e3c2c9ddfd7ad4e67
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-12-05 19:17:15 -0500

    Connected to mongoDB

---
+++ database.js
@@ -0,0 +1,87 @@
+import 'dotenv/config';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export class ConstellationDatabase {
+  constructor(dburl) {
+    this.dburl = dburl;
+  }
+
+  async connect() {
+    
+    this.client = new MongoClient(this.dburl, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      });
+
+    // Get the database.
+    this.db = this.client.db('Constellations');
+    
+    await this.init();
+  }
+
+  async init() {
+    this.collection = this.db.collection('Constellation');
+
+    const count = await this.collection.countDocuments();
+
+    if (count === 0) {
+      await this.collection.insertOne([
+        {fullName:"Andromeda",
+        abbreviations:"And",
+        origin:"ancient (Ptolemy)",
+        meaning:"Andromeda (The chained maiden or princess)",
+        brightestStar:"Alpheratz",
+        url:"https://upload.wikimedia.org/wikipedia/commons/8/87/Andromeda_IAU.svg",
+        visible:"Visible at latitudes between +90° and −40°.",
+        starNum: 16,
+        area:722}
+      ]);
+    }
+  }
+  // Close the pool.
+  async close() {
+    this.client.close();
+  }
+
+//   // CREATE a user in the database.
+//   async createPerson(id, name, age) {
+//     const res = await this.collection.insertOne({ _id: id, name, age });
+//     // Note: the result received back from MongoDB does not contain the
+//     // entire document that was inserted into the database. Instead, it
+//     // only contains the _id of the document (and an acknowledged field).
+//     return res;
+//   }
+
+  // READ a user from the database.
+  async readPerson(id) {
+    const res = await this.collection.findOne({ fullName: id });
+    return res;
+  }
+
+//   // UPDATE a user in the database.
+//   async updatePerson(id, name, age) {
+//     const res = await this.collection.updateOne(
+//       { _id: id },
+//       { $set: { name, age } }
+//     );
+//     return res;
+//   }
+
+//   // DELETE a user from the database.
+//   async deletePerson(id) {
+//     // Note: the result received back from MongoDB does not contain the
+//     // entire document that was deleted from the database. Instead, it
+//     // only contains the 'deletedCount' (and an acknowledged field).
+//     const res = await this.collection.deleteOne({ _id: id });
+//     return res;
+//   }
+
+  // READ all people from the database.
+  async readAllPeople() {
+    const res = await this.collection.find({}).toArray();
+    return res;
+  }
+}
--- server.js
+++ server.js
@@ -1,54 +1,90 @@
 console.log('Server-side code running');
 
-const express = require('express');
-const { Client } = require('pg');
+// const express = require('express');
+// const MongoClient = require('mongodb').MongoClient;
+import 'dotenv/config';
+import express from 'express';
+import { ConstellationDatabase } from './database.js';
 
-const app = express();
-const port = 3000;
-app.use(express.static('public'));
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Constellations',
-  password: 'AppaMomo2025',
-  port: 5432,
-});
-
-client.connect();
-
-app.get('/constellation', async (req, res) => {
-  try {
-    const { rows } = await client.query('SELECT fullName FROM constellation');
-    res.send(rows);
-    console.log(rows);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error retrieving users from database');
+class ConsetllationServer {
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
   }
-});
 
-// const tableName = 'constellation';
-// const query = `SELECT * FROM ${tableName}`;
+  async initRoutes() {
+    // Note: when using arrow functions, the "this" binding is lost.
+    const self = this;
 
-// client.query(query, (err, result) => {
-//   if (err) {
-//     console.error('Error executing query', err);
-//     client.end();
-//     return;
-//   }
+    // this.app.get('/person/create', async (req, res) => {
+    //   try {
+    //     const { id, name, age } = req.query;
+    //     const person = await self.db.createPerson(id, name, age);
+    //     res.send(JSON.stringify(person));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
 
-//   const rows = result.rows;
-//   console.log('Rows from the table:', rows);
+    this.app.get('/read', async (req, res) => {
+      try {
+        const { id } = req.query;
+        const name = await self.db.readPerson(id);
+        res.send(JSON.stringify(name));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
 
-//   client.end();
-// });
+    // this.app.get('/person/update', async (req, res) => {
+    //   try {
+    //     const { id, name, age } = req.query;
+    //     const person = await self.db.updatePerson(id, name, age);
+    //     res.send(JSON.stringify(person));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
 
-app.listen(port, () => {
-  console.log('listening on 3000 on http://localhost:3000/');
-});
+    // this.app.get('/person/delete', async (req, res) => {
+    //   try {
+    //     const { id } = req.query;
+    //     const person = await self.db.deletePerson(id);
+    //     res.send(JSON.stringify(person));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
 
-// serve the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+    this.app.get('/person/all', async (req, res) => {
+      try {
+        const people = await self.db.readAllPeople();
+        res.send(JSON.stringify(people));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  async initDb() {
+    this.db = new ConstellationDatabase(this.dburl);
+    await this.db.connect();
+  }
+
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 3000;
+    this.app.use(express.static('public'));
+    this.app.listen(port, () => {
+      console.log(`PeopleServer listening on port ${port}!`);
+    });
+    this.app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    });
+  }
+}
+
+const server = new ConsetllationServer(process.env.DATABASE_URL);
+server.start();
+
