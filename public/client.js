be50e46265ad431a4482ccb95eef97f03ba71841
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-12-09 17:28:47 -0500

    Display information to rectangle

--- public/client.js
+++ public/client.js
@@ -8,19 +8,43 @@ function scrollToSection(sectionId) {
   }
 }  
 
+let search = document.getElementById('constellationSearch').value;
 let button = document.getElementById('searchButton');
-button.addEventListener('click', function(e) {
-  console.log('Button was clicked');
+button.addEventListener('click', () => {
+  // console.log(search);
 
-  fetch('/constellation', {method: 'GET'})
-  .then(function(response) {
-    if(response.ok) {
-      console.log('click was recorded');
-      return;
-    }
-    throw new Error('Request failed.');
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+  fetch('/constellation', {method: 'GET'})   
+    .then(response => response.json())
+    .then(data => {
+
+      const dataString = JSON.stringify(data);
+      const jsonData = JSON.parse(dataString);
+      
+      const fullName = jsonData.fullName;
+      const abbreviations = jsonData.abbreviations;
+      const origin = jsonData.origin;
+      const meaning = jsonData.meaning;
+      const brightestStar = jsonData.brightestStar;
+      const url = jsonData.url;
+      const visible = jsonData.visible;
+      const starNum = jsonData.starNum;
+      const area = jsonData.area;
+
+      const divElement = document.getElementById('constellationInfo');
+
+      divElement.innerHTML = `
+      <h2>${fullName}</h2>
+      <img src="${url}" alt="Description of the image" width="500" height="500">
+      <p><strong>Abbreviations:</strong> ${abbreviations}</p>
+      <p><strong>Origin:</strong> ${origin}</p>
+      <p><strong>Meaning:</strong> ${meaning}</p>
+      <p><strong>Brightest Star:</strong> ${brightestStar}</p>
+      <p><strong>Visible:</strong> ${visible}</p>
+      <p><strong>Star Number:</strong> ${starNum}</p>
+      <p><strong>Area:</strong> ${area}</p>
+    `;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
--- server.js
+++ server.js
@@ -56,14 +56,29 @@ class ConsetllationServer {
     // });
 
     this.app.get('/constellation', async (req, res) => {
+      //const options = req.body;
       try { 
         const {name} = req.query;
         const constellation = await self.db.readConstellation(name);
         console.log(constellation);
+        res.send(JSON.stringify(constellation));
+        //console.log(options.search);
       } catch (err) {
         res.status(500).send(err);
       }
     });
+
+    // this.app.post('/myform', function(req, res){ 
+      
+    //   const options = req.body;
+    //   console.log(options);
+    //   try {
+    //     console.log(req.body.mytext);
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    //   res.send('Data received: ' + req.body.mytext);
+    // });
 
     this.app.get('/person/all', async (req, res) => {
       try {
@@ -74,6 +89,7 @@ class ConsetllationServer {
         res.status(500).send(err);
       }
     });
+
   }
 
   async initDb() {
@@ -85,7 +101,13 @@ class ConsetllationServer {
     await this.initRoutes();
     await this.initDb();
     const port = process.env.PORT || 3000;
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({
+      extended: true
+    }));
     this.app.use(express.static('public'));
+
     this.app.listen(port, () => {
       console.log(`PeopleServer listening on port ${port}! on http://localhost:${port}`);
     });
